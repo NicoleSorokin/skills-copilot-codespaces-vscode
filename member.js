1a454e58cde305c6bd6a5162f4804a0d26abd8af
NicoleSorokin <85652961+NicoleSorokin@users.noreply.github.com>
2024-05-09 15:37:44 +0000

    Copilt second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+    // Path: member.js
+    var member = {
+        id: 1,
+        name: "John Doe",
+        skills: ["C#", "SQL Server", "ASP.NET"]
+    };
+
+    return member;
+}
